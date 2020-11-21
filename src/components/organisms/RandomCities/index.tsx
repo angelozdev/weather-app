14192a0567adda33c0cf5bb669aab7b4fbe8c414
Angelo Zambrano <angelozam17@gmail.com>
2020-11-20 20:02:39 -0500

    feat: code and styles of random cities component

--- src/components/organisms/RandomCities/index.tsx
+++ src/components/organisms/RandomCities/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Plus } from '../../Icons'
+
+/* Components */
+import { Map, Plus } from '../../Icons'
 
 /* Styles */
 import './style.sass'
 
 function RandomCities() {
   /* States */
-  const SIZE = '25px'
+  const SIZE = '20px'
 
   return (
     <div className="random-cities">
@@ -14,11 +16,29 @@ function RandomCities() {
         <Plus height={SIZE} width={SIZE} />
         <span>Top Reviewers</span>
         <div className="random-cities__reviewers">
-          <img src="https://picsum.photos/50" alt="reviewer 1" />
-          <img src="https://picsum.photos/50" alt="reviewer 2" />
-          <img src="https://picsum.photos/50" alt="reviewer 3" />
-          <img src="https://picsum.photos/50" alt="reviewer 4" />
+          <img src="https://picsum.photos/20?random=1" alt="reviewer 1" />
+          <img src="https://picsum.photos/20?random=2" alt="reviewer 2" />
+          <img src="https://picsum.photos/20?random=3" alt="reviewer 3" />
+          <img src="https://picsum.photos/20?random=4" alt="reviewer 4" />
         </div>
+      </div>
+
+      <div className="random-cities__body">
+        <figure>
+          <img src="https://picsum.photos/300?random=1" alt="image city 1" />
+          <figcaption>
+            <Map width={SIZE} height={SIZE} />
+            <span>Art Science</span>
+          </figcaption>
+        </figure>
+
+        <figure>
+          <img src="https://picsum.photos/300?random=2" alt="image city 2" />
+          <figcaption>
+            <Map width={SIZE} height={SIZE} />
+            <span>Lorem ipsum</span>
+          </figcaption>
+        </figure>
       </div>
     </div>
   )
