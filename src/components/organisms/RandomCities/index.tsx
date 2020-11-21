3fa053e479043c5dd74f09c9e4c6a6d267e138c0
Angelo Zambrano <angelozam17@gmail.com>
2020-11-20 19:20:33 -0500

    feat(random-cities): header + styles

--- src/components/organisms/RandomCities/index.tsx
+++ src/components/organisms/RandomCities/index.tsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import { Plus } from '../../Icons'
+
+/* Styles */
+import './style.sass'
 
 function RandomCities() {
-  return <div>Hola</div>
+  /* States */
+  const SIZE = '25px'
+
+  return (
+    <div className="random-cities">
+      <div className="random-cities__header">
+        <Plus height={SIZE} width={SIZE} />
+        <span>Top Reviewers</span>
+        <div className="random-cities__reviewers">
+          <img src="https://picsum.photos/50" alt="reviewer 1" />
+          <img src="https://picsum.photos/50" alt="reviewer 2" />
+          <img src="https://picsum.photos/50" alt="reviewer 3" />
+          <img src="https://picsum.photos/50" alt="reviewer 4" />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default RandomCities
